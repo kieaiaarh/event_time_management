6fe9e04c0da1a1381c9bb726fbf563be307464d6
kieaiaarh <kieaiaarh@gmail.com>
2022-05-18 14:46:01 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png" />
+    　<EventTimeManagement msg="Hello Composition API in CodeSandbox!" />
+  </div>
+</template>
+
+<script>
+import EventTimeManagement from "./components/EventTimeManagement.vue";
+export default {
+  name: "App",
+  components: {
+    EventTimeManagement,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
